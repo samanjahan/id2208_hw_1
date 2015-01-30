7333d1fed504219a5dcd2278dfc248bfadb3ac5d
Saman Jahanbakhsh <samanj@kth.se>
2015-01-30 12:51:40 +0100

    first commit

---
+++ src/java/Dom.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author syst3m
+ */
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import java.io.File;
+public class Dom {
+   // File xmlfile = new File
+    public static void main(String[] args) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); 
+    }
+}
