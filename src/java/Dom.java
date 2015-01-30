d9da95c3fd78e785ea8d34cd132fa51b2e13df0d
Saman Jahanbakhsh <samanj@kth.se>
2015-01-31 00:15:03 +0100

    dom

--- src/java/Dom.java
+++ src/java/Dom.java
@@ -9,12 +9,130 @@
  * @author syst3m
  */
 
-import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import javax.xml.transform.TransformerFactory;
+import org.w3c.dom.Element;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 public class Dom {
-   // File xmlfile = new File
-    public static void main(String[] args) {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); 
+  static final String W3C_XML_SCHEMA = "cv.xsd";
+    public static void main(String[] args) throws ParserConfigurationException, TransformerConfigurationException, TransformerException {
+        
+        File cvFile = new File("web/WEB-INF/cv.xml");
+        File companyFile = new File("web/WEB-INF/company.xml");
+        File recordsFile = new File("web/WEB-INF/employmentrecords.xml");
+        File transcriptFile = new File("web/WEB-INF/transcript.xml");
+        
+        
+        DocumentBuilderFactory factoryProfile = DocumentBuilderFactory.newInstance();
+        
+        DocumentBuilderFactory factoryCv = DocumentBuilderFactory.newInstance();
+        DocumentBuilderFactory factoryCompany = DocumentBuilderFactory.newInstance();
+        DocumentBuilderFactory factoryRecord = DocumentBuilderFactory.newInstance();
+        DocumentBuilderFactory factoryTranscript = DocumentBuilderFactory.newInstance();
+         
+         
+        DocumentBuilder dBuilderCv = factoryCv.newDocumentBuilder();
+        DocumentBuilder dBuilderCompany = factoryCompany.newDocumentBuilder();
+        DocumentBuilder dBuilderRecord = factoryRecord.newDocumentBuilder();
+        DocumentBuilder dBuilderTranscript = factoryTranscript.newDocumentBuilder();
+        DocumentBuilder docBuilderProfile = factoryProfile.newDocumentBuilder();
+
+
+        Document docCv = null;
+        Document docCompany = null;
+        Document docRecord = null;
+        Document docTranscript = null;
+        Document docProfile = docBuilderProfile.newDocument();
+        
+        Element rootElement = docProfile.createElement("profile");
+        docProfile.appendChild(rootElement);
+        
+        
+        
+    
+        try {
+            docCv = dBuilderCv.parse(cvFile);
+            docCompany = dBuilderCv.parse(companyFile);
+            docTranscript = dBuilderTranscript.parse(transcriptFile);
+        } catch (SAXException ex) {
+            Logger.getLogger(Dom.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Dom.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+   
+        NodeList nListCv = docCv.getElementsByTagName("person");
+        NodeList nListTranscript = docTranscript.getElementsByTagName("university");
+        NodeList nListCompany = docCompany.getElementsByTagName("name");
+        NodeList nListRecord = docCv.getElementsByTagName("name");
+        
+        for(int i = 0 ; i < nListCv.getLength(); ++i ){
+            Node nNode = nListCv.item(i);
+            Element eElement = (Element) nNode;
+            
+            Element personElement = docProfile.createElement("person");
+            Element nameElement = docProfile.createElement("name");
+            Element surenameElement = docProfile.createElement("surename");
+            
+            String personName = eElement.getElementsByTagName("name").item(0).getTextContent();
+            rootElement.appendChild(personElement);
+            
+            nameElement.appendChild(docProfile.createTextNode(personName));
+            personElement.appendChild(nameElement);
+            
+            surenameElement.appendChild(docProfile.createTextNode(eElement.getElementsByTagName("surename").item(0).getTextContent()));
+            personElement.appendChild(surenameElement);
+            
+            Element universityElement = docProfile.createElement("university");
+            personElement.appendChild(universityElement);
+            
+            for(int j = 0 ; j < nListTranscript.getLength(); ++j){
+                
+                
+                Element universityname = docProfile.createElement("universityname");
+                Element degreeElement = docProfile.createElement("degree");               
+
+
+                System.out.println("j" + j);
+                Node nNodeTrans = nListTranscript.item(j);
+                
+                Element eElementtrans = (Element) nNodeTrans;
+                String personNamefromtranscript = eElementtrans.getElementsByTagName("name").item(0).getTextContent() ;
+               
+                if(personNamefromtranscript.equals(personName)){
+                    universityname.appendChild(docProfile.createTextNode(eElementtrans.getElementsByTagName("universityname").item(0).getTextContent()));
+                    degreeElement.appendChild(docProfile.createTextNode(eElementtrans.getElementsByTagName("degree").item(0).getTextContent()));
+                    universityElement.appendChild(universityname);
+                    universityElement.appendChild(degreeElement);
+                }
+                
+                
+                
+                
+                Element degree = docProfile.createElement("degree");
+                
+                
+            }
+        }
+        
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(docProfile);
+        StreamResult result =  new StreamResult(new File("web/WEB-INF/pr.xml"));
+        transformer.transform(source, result);
     }
 }
